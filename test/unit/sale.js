3c1c5c5aec5762cd35d93d7f527865b8fdade486
Easton Gorishek <ieaston26@gmail.com>
2018-07-30 10:58:31 -0700

    good model for sale done

---
+++ test/unit/sale.js
@@ -0,0 +1,42 @@
+const { assert } = require('chai');
+const { getErrors } = require('./helpers');
+const Sale = require('../../lib/models/sale');
+const { Types } = require('mongoose');
+
+describe('Sale model', () => {
+    
+    it('validates a good model', () => {
+        const data = {
+            bar: Types.ObjectId(),
+            customer: Types.ObjectId(),
+            drinks: [{
+                type: 'beer',
+                name: 'boneyard',
+                price: 4,
+                quantity: 1
+            }],
+            food:[{
+                type: 'starter',
+                price: 4,
+                quantity: 1
+            }],
+            totalAmountSpent: 8
+        };
+
+        const sale = new Sale(data);
+        const json = sale.toJSON();
+        delete json._id;
+        delete json.drinks[0]._id;
+        delete json.food[0]._id;
+        console.log('****JSON****', json);
+        console.log('****DATA****', data);
+        assert.deepEqual(json, data);
+        assert.isUndefined(sale.validateSync());
+    });
+
+    it('validates required fields (drinks, food, amount spent)', () => {
+        const sale = new Sale({});
+        const errors = getErrors(sale.validateSync(), 1);
+        assert.equal(errors.drinks.kind, 'required');
+    });
+});
