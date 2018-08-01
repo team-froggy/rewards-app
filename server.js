cfbd9150389afe571f357e1fe1716a1ee31b26e8
Kevin Grayson <k.grayson.ms@gmail.com>
2018-08-01 12:34:13 -0700

    Testing heroku

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+const MONGODB_URI = process.env.MONGODB_URI;
 const http = require('http');
 const app = require('./lib/app');
 const connect = require('./lib/util/connect');
 
-connect('mongodb://localhost:27017/team_froggy_test');
+connect(MONGODB_URI);
 
 const server = http.createServer(app);
 const port = process.env.port || 3000;
