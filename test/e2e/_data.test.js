c5df2b553014dc0d32c44511e23d8dc171c7974b
Kevin Grayson <k.grayson.ms@gmail.com>
2018-08-02 14:20:44 -0700

    Seed data updated

--- test/e2e/_data.test.js
+++ test/e2e/_data.test.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 const mongoose = require('mongoose');
 const barsDataFile = join(__dirname, '../../lib/data/bars-data.json');
 const usersDataFile = join(__dirname, '../../lib/data/users-data.json');
-const salesDataFile = join(__dirname, '../../lib/data-backup/sales-data.json');
+const salesDataFile = join(__dirname, '../../lib/data/sales-data.json');
 
 describe.only('Seed data API', () => {
 
@@ -14,7 +14,7 @@ describe.only('Seed data API', () => {
         // execSync('mongoimport -h ds263791.mlab.com:63791 -d heroku_p43lq6vx -c bars -u heroku_p43lq6vx -p q7k078nvikkva4bmmk74no52fj --drop --file lib/data/bars-data.json');
         // execSync('mongoimport -h ds263791.mlab.com:63791 -d heroku_p43lq6vx -c sales -u heroku_p43lq6vx -p q7k078nvikkva4bmmk74no52fj --drop --file lib/data/sales-data.json');
         execSync(`mongoimport --db ${mongoose.connection.name} --collection users --drop --file ${usersDataFile}`);
-        execSync(`mongoimport --db ${mongoose.connection.name} --collection bars  --drop --file ${barsDataFile}`);
+        execSync(`mongoimport --db ${mongoose.connection.name} --collection bars --drop --file ${barsDataFile}`);
         execSync(`mongoimport --db ${mongoose.connection.name} --collection sales --drop --file ${salesDataFile}`);
     });
     
